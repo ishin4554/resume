208f164f3b4b45e5307a54db797745a2579f0982
Min Wei <winder3689@gmail.com>
2019-11-10 23:23:11 +0800

    Update pdf

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -26,7 +26,7 @@ const Cta = styled.div`
 const About = props => pug`
   Cta
     h1 Contact Me
-    p 感謝您的撥冗閱讀。
+    p 感謝您閱讀到最後。
     p 工作之餘的我是個 #[span &#127916] 熱愛動畫、 #[Link(href='https://vimeo.com/minw' target='_blank') 做動畫] 的創作者，也喜歡寫些文章或舉辦活動分享自己的 #[span &#127881] #[Link(href='https://medium.com/@minw') 學習經驗]。
     p 目前求職中，如果您對我的經歷有興趣，可以 #[Link(href='mailto:minw.contacts@gmail.com' target='_blank') 透過 Email 聯繫我]、或看看 #[Link(href='./resume.pdf' target='_blank') 我的履歷]。
 `
